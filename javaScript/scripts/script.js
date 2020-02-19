59b0546fc1f93671c811ef0562619d2b47911e2a
jitender85 <jitenderwebdesigner@gmail.com>
2020-02-19 22:30:35 +0530

    plz see

---
+++ javaScript/scripts/script.js
@@ -0,0 +1,19 @@
+/*my first javascript code*/
+ function myFunction () {
+    document.getElementById("first").innerHTML = "Paragraph changed.";
+ }
+
+document.getElementById("demo1").innerHTML = 5+6;
+
+/*window.alert(5+61);*/
+
+console.log(15+6);
+
+var a = 42
+var b = 56
+var c = a + b
+
+document.getElementById('statement').innerHTML = "The value of c is "+ c +".";
+
+a = 10; b = 25; c = a+b;
+ document.getElementById('semicolon').innerHTML =  c;
